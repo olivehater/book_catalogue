30c995cf9a6a46ce31f64acdc2cbc0e5f7705004
Oliwia Gajewska <gajewska.oli@gmail.com>
2020-05-23 19:11:58 +0200

    delete favourites

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -5,10 +5,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Favourite;
 use App\Repository\FavouriteRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -49,6 +52,50 @@ class UserController extends AbstractController
             [
                 'favourite' => $favourite,
                 'user' => $user,
+            ]
+        );
+    }
+
+    /**
+     * Delete favourite action.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request             HTTP request
+     * @param \App\Entity\Favourite                     $favourite           Favourite entity
+     * @param \App\Repository\FavouriteRepository       $favouriteRepository Favourite repository
+     *
+     * @return \Symfony\Component\HttpFoundation\Response HTTP response
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     *
+     * @Route(
+     *     "/favourite/{id}/delete",
+     *     methods={"GET", "DELETE"},
+     *     name="favourite_delete"
+     * )
+     */
+    public function deleteFavourite(Request $request, Favourite $favourite, FavouriteRepository $favouriteRepository): Response
+    {
+        $form = $this->createForm(FormType::class, $favourite, ['method' => 'DELETE']);
+        $form->handleRequest($request);
+
+        if ($request->isMethod('DELETE') && !$form->isSubmitted()) {
+            $form->submit($request->request->get($form->getName()));
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $favouriteRepository->delete($favourite);
+
+            $this->addFlash('success', 'message_deleted_successfully');
+
+            return $this->redirectToRoute('user_favourite_index');
+        }
+
+        return $this->render(
+            'user/delete_favourite.html.twig',
+            [
+                'form' => $form->createView(),
+                'favourite' => $favourite,
             ]
         );
     }
